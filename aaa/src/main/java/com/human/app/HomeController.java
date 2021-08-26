4164521f2cc58ea7f77487c9264d338c357e93d7
chiochiwo <ccu12345@naver.com>
2021-08-26 20:10:18 +0900

    수정 커밋

--- aaa/src/main/java/com/human/app/HomeController.java
+++ aaa/src/main/java/com/human/app/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -26,11 +27,6 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home1(Locale locale, Model m) {		
-		m.addAttribute("m_name", "jenny,jisoo");		
-		return "members";
-	}
-	@RequestMapping(value = "/a", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -40,48 +36,56 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("myName", "Choi");
-		model.addAttribute("myHome", "pyeongtaek");
-		model.addAttribute("myMobile", "01055554444");
-		
-		return "home";
+		model.addAttribute("myName", "Lim Eunbi");
+		model.addAttribute("address", "봉서산샛길 64");
+		model.addAttribute("mobile", "01080753708");
+		return "members";
 	}
-	@RequestMapping(value = "/getinfo")
+	@RequestMapping("/getinfo")
 	public String getInfo() {
 		return "getinfo";
 	}
 	
-	@RequestMapping(value = "/info")
-	public String doInfo(HttpServletRequest hsr, Model model) {		
-		String uid=hsr.getParameter("userid");	
-		String addr=hsr.getParameter("address");
-		System.out.println("uid="+uid);
-		System.out.println("addr="+addr);
-		model.addAttribute("loginid",uid);
-		model.addAttribute("region",addr);
-		// userid -> uid -> loginid, address->addr->region
+	@RequestMapping("/info")
+	public String doInfo(@RequestParam("userid") String id,@RequestParam("address") String addr, Model model) {
+//		String id=hsr.getParameter("userid");
+//		String addr=hsr.getParameter("address");
+//		
+		model.addAttribute("loginid", id);
+		model.addAttribute("region", addr);
+		
 		return "viewinfo";
 	}
-	
-	@RequestMapping(value = "/choose")
+	@RequestMapping("/choose")
 	public String doChoose() {
 		return "choose";
 	}
-	@RequestMapping(value = "/selected")
-	public String doJob(HttpServletRequest hsr, Model model) {
-		String strPath=hsr.getParameter("path");
+	
+	@RequestMapping("/selected")
+//	public String doJob(HttpServletRequest hsr, Model model) {
+	public String doJob(@RequestParam("path") String strPath, Model model) {
+//		String strPath=hsr.getParameter("path");
 		if(strPath.equals("login")) {
 			return "getinfo";
-		} else if(strPath.equals("newbie")) {
+		}else if(strPath.equals("newbie")) {
 			return "newbie";
-		} else {
-			return "choose";
 		}
+		return "choose";
 	}
-	@RequestMapping(value = "/today/{address}/{userid}")
-	public String showNumber(@PathVariable String userid,@PathVariable String address,Model model) {
-		model.addAttribute("uid",userid);
-		model.addAttribute("addr",address);
+	
+	//Command 객체
+//	public String doJob(ParamList pl, Model model) {
+//		model.addAttribute("userid", pl.userid);
+//		model.addAttribute("addr", pl.address);
+//		return "";
+//	}
+	//PathVariable
+	@RequestMapping("/today/{year}/{month}/{day}")
+	public String showToday(@PathVariable int year, @PathVariable int month, @PathVariable int day, Model model) {
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
 		return "today";
 	}
+	
 }
