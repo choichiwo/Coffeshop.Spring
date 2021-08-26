59e5a135bf8a546ee86e36b3bcc05e0a40b3e6b7
chiochiwo <ccu12345@naver.com>
2021-08-26 09:11:19 +0900

    1교시 커밋

--- aaa/src/main/java/com/human/app/MyController.java
+++ aaa/src/main/java/com/human/app/MyController.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
-	@RequestMapping("/1")
-	public String ∏ﬁº“µÂ¿Ã∏ß( Model model) {		
-		model.addAttribute("mobile", "2134556");		
-		return "contanas";  //home    .jspª˝∑´
-	}
+   
+//   @RequestMapping("/contactus")
+//   public String method1(Model model) {             // Model class Ïù¥Ïö©.
+//      model.addAttribute("mobile", "01010101010");
+//      return "contactus";
+//      // /WEB-INF/views/contactus.jsp
+//   }
+   
+   @RequestMapping("/contactus")
+   public ModelAndView method2() {
+      ModelAndView mov = new ModelAndView();
+      mov.addObject("mobile","041-561-1122");
+      mov.setViewName("contactus");
+      return mov;
+   }
 }
